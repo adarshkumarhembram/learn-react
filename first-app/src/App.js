dcec508af42e9281e11dc28d105b3c77fad5cc6a
adarshkumarhembram <hembramadarshkumar@gmail.com>
2024-10-05 11:01:42 +0530

    learning about jsx

---
+++ first-app/src/App.js
@@ -0,0 +1,19 @@
+import CatCaed from './CatCaed';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <button id='btn'>
+        <span>
+          submit
+          
+        </span>
+      </button>
+      <CatCaed/>
+      <CatCaed/>
+    </div>
+  );
+}
+
+export default App;
